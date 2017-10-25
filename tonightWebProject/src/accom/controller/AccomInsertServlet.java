b7decc9a6b59eb799ebb320789de40d45eb7cfa0
hyejungk93 <32886898+hyejungk93@users.noreply.github.com>
2017-10-25 15:18:20 +0900

    1025
    1025

--- tonightWebProject/src/accom/controller/AccomInsertServlet.java
+++ tonightWebProject/src/accom/controller/AccomInsertServlet.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
 import accom.model.dao.AccomDao;
 import accom.model.service.AccomService;
 import accom.model.vo.Accommodation;
@@ -33,6 +35,26 @@ public class AccomInsertServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//숙소 등록 처리용 컨트롤러
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		
+		//업로드할 파일의 용량 제한 : 10Mbyte 로 제한한다면
+		int maxSize = 1024 * 1024 * 10;
+		
+		//enctype="multipart/form-data" 로 전송되었는지 확인
+		RequestDispatcher view = null;
+		if(!ServletFileUpload.isMultipartContent(request)){
+			view = request.getRequestDispatcher("views/accom/accomError.jsp");
+			request.setAttribute("message", "form enctype 속성 사용 안 함!");
+			view.forward(request, response);
+		}
+		
+		//해당 컨테이너에서 구동중인 웹 애플리케이션의 루트 경로 알아냄
+		String root = request.getSession().getServletContext().getRealPath("/");
+		//업로드되는 파일이 저장될 폴더명과 루트 경로 연결 처리
+		String savePath = root + "buploadfiles";
+		//web/buploadfiles 로 지정됨
+		
 		Accommodation accom = null;
 		new AccomService().insertAccom(accom);
 		
